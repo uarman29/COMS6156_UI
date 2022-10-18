24e603c3503f5a5aa9b84d01eea8a6d5535dc883
Arman <uarman2215@gmail.com>
2022-10-18 15:49:47 -0400

    Added cards and card views

---
+++ src/app/components/card-view/card-view.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackendServiceService, Card } from 'src/app/services/backend-service.service';
+
+@Component({
+  selector: 'app-card-view',
+  templateUrl: './card-view.component.html',
+  styleUrls: ['./card-view.component.css']
+})
+export class CardViewComponent implements OnInit {
+
+  card!:Card;
+  id!:number;
+
+  cardForm = this.fb.group({
+    user_id: [0],
+    card_no: [''],
+    expiration_date: [''],
+    cvv: ['']
+  });
+
+  get user_id() {
+    return this.cardForm.get('user_id') as FormControl;
+  }
+
+  get card_no() {
+    return this.cardForm.get('card_no') as FormControl;
+  }
+
+  get expiration_date() {
+    return this.cardForm.get('expiration_date') as FormControl;
+  }
+
+  get cvv() {
+    return this.cardForm.get('cvv') as FormControl;
+  }
+
+  constructor(private backendService:BackendServiceService, private fb:FormBuilder, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.backendService.getCard(this.id).subscribe(card =>{
+      if(card === undefined)
+        this.router.navigate(['/cards']);
+      else
+        this.card = card;
+      this.user_id.setValue(this.card.user_id);
+      this.card_no.setValue(this.card.card_no);
+      this.expiration_date.setValue(this.card.expiration_date);
+      this.cvv.setValue(this.card.cvv);
+    });
+  }
+
+  onUpdateSubmit() {
+    if(!this.cardForm.valid){
+      return;
+    }
+    let c:Card = {card_id: this.card.card_id, user_id: this.user_id.value, card_no: this.card_no.value, expiration_date: this.expiration_date.value, cvv: this.cvv.value};
+    this.backendService.updateCard(c);
+  }
+
+  onDelete() {
+    this.backendService.deleteCard(this.card.card_id);
+    this.router.navigate(['/cards']);
+  }
+
+}
---
+++ src/app/components/cards-view/cards-view.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { BackendServiceService, Card } from 'src/app/services/backend-service.service';
+
+@Component({
+  selector: 'app-cards-view',
+  templateUrl: './cards-view.component.html',
+  styleUrls: ['./cards-view.component.css']
+})
+export class CardsViewComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  cards: Card[] = [];
+  displayedColumns:string[] = ["card_id", "user_id", "card_no", "expiration_date", "cvv"];
+  dataSource!:MatTableDataSource<Card>;
+
+  cardForm = this.fb.group({
+    user_id: [0],
+    card_no: [''],
+    expiration_date: [''],
+    cvv: ['']
+  });
+
+  get user_id() {
+    return this.cardForm.get('user_id') as FormControl;
+  }
+
+  get card_no() {
+    return this.cardForm.get('card_no') as FormControl;
+  }
+
+  get expiration_date() {
+    return this.cardForm.get('expiration_date') as FormControl;
+  }
+
+  get cvv() {
+    return this.cardForm.get('cvv') as FormControl;
+  }
+
+
+  constructor(private backendService:BackendServiceService, private fb:FormBuilder) { 
+  }
+
+  ngOnInit(): void {
+    this.backendService.getCards().subscribe(cards =>{
+      this.cards = cards;
+      this.dataSource = new MatTableDataSource<Card>(this.cards);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  onSubmit() {
+    if(!this.cardForm.valid){
+      return;
+    }
+    let c:Card = {card_id: Math.max(...this.cards.map(card => card.card_id), 0) + 1, user_id: this.user_id.value, card_no: this.card_no.value, expiration_date: this.expiration_date.value, cvv: this.cvv.value};
+    this.backendService.addCard(c);
+  }
+
+}
